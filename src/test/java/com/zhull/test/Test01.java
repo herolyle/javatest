3fc080dedbe97b7a65e2cd54c89cc3db938bfa47
lyle.zhu <sdzhull@126.com>
2013-01-09 13:15:36 +0800

    测试

---
+++ src/test/java/com/zhull/test/Test01.java
@@ -0,0 +1,27 @@
+package com.zhull.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.lang.System;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhulilei
+ * Date: 13-1-8
+ * Time: 下午8:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test01 {
+
+
+    @Test
+    public void test_01(){
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void test_02(){
+        assertEquals(1,2);
+    }
+}
